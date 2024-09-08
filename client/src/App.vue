13c59d378fb12ceec254b742b93d4a1958ed8faa
Jan Warnecke-Hasselberg <jhasselberg@kpler.com>
2024-09-08 17:15:06 +0200

    ♻️ (client): put footer in BaseFooter.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,19 +1,7 @@
 <template>
   <BaseNavbar />
   <router-view />
-  <footer class="footer p-10 text-neutral-content bg-base-300">
-    <div>
-      <span class="footer-title">Links</span>
-      <a href="/impressum" class="link link-hover">Impressum</a>
-      <a href="/privacy" class="link link-hover">Privacy Policy</a>
-      <a href="https://status.jnhssg.de/status/likedeeler" class="link link-hover">System Status</a>
-    </div>
-    <div class="divider"></div>
-    <div class="mt-4">
-      <p>Made with <span style="color: #e25555">&#9829;</span> in Rostock.</p>
-      <p class="text-gray-500">{{ VERSION }}</p>
-    </div>
-  </footer>
+  <BaseFooter />
 </template>
 
 <script>
@@ -21,10 +9,11 @@ import { defineComponent } from 'vue'
 
 import { VERSION } from '@/configurations/general.conf'
 import BaseNavbar from "@/components/BaseNavbar.vue";
+import BaseFooter from "@/components/BaseFooter.vue";
 
 export default defineComponent({
   name: 'App',
-  components: {BaseNavbar},
+  components: {BaseFooter, BaseNavbar},
   setup() {
     return {
       VERSION
---
+++ client/src/components/BaseFooter.vue
@@ -0,0 +1,33 @@
+<template>
+  <footer class="footer p-10 text-neutral-content bg-base-300">
+    <div>
+      <span class="footer-title">Links</span>
+      <a href="/impressum" class="link link-hover">Impressum</a>
+      <a href="/privacy" class="link link-hover">Privacy Policy</a>
+      <a href="https://status.jnhssg.de/status/likedeeler" class="link link-hover">System Status</a>
+    </div>
+    <div class="divider"></div>
+    <div class="mt-4">
+      <p>Made with <span style="color: #e25555">&#9829;</span> in Rostock.</p>
+      <p class="text-gray-500">{{ VERSION }}</p>
+    </div>
+  </footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { VERSION } from '@/configurations/general.conf'
+
+export default defineComponent({
+  name: 'BaseFooter',
+  setup() {
+    return {
+      VERSION
+    }
+  }
+})
+</script>
+
+<style scoped>
+/* Add any styles if necessary */
+</style>
