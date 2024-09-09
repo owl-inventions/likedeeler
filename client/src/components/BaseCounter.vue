252faa8db9a5050ec160f361e06846b225206600
Jan Warnecke-Hasselberg <jhasselberg@kpler.com>
2024-09-09 16:49:41 +0200

    ✨ (client): add counter comp

---
+++ client/src/components/BaseCounter.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="flex gap-5">
+    <div>
+      <span class="countdown font-mono text-4xl">
+        <span :style="{ '--value': 15 }"></span>
+      </span>
+      days
+    </div>
+    <div>
+      <span class="countdown font-mono text-4xl">
+        <span :style="{ '--value': 10 }"></span>
+      </span>
+      hours
+    </div>
+    <div>
+      <span class="countdown font-mono text-4xl">
+        <span :style="{ '--value': 24 }"></span>
+      </span>
+      min
+    </div>
+    <div>
+      <span class="countdown font-mono text-4xl">
+        <span :style="{ '--value': counter }"></span>
+      </span>
+      sec
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'BaseCounter',
+  setup() {
+    const counter = ref(0)
+
+    return {
+      counter
+    }
+  }
+})
+</script>
+
+<style scoped>
+.countdown {
+  /* Add any additional styles if needed */
+}
+</style>
